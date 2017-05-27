1965d4578a216593f9fc2e47b730a1c49d60a42b
Anton Tarianyk <anton_tarianyk@epam.com>
2017-05-27 13:31:57 +0300

    Dev2

--- M1/Hw1/cdp-git/src/main/java/com/epam/m1/hw1/first/First.java
+++ M1/Hw1/cdp-git/src/main/java/com/epam/m1/hw1/first/First.java
@@ -3,11 +3,11 @@ package com.epam.m1.hw1.first;
 public class First {
 
     public String methodOne() {
-        return "ST1 Dev1";
+        return "ST2 Feature1";
     }
 	
 	public String methodTwo() {
-        return "c1m2";
+        return "ST2 Dev2";
     }
 
 }
